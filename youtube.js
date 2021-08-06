17eb66b693c39bd22ec9bf0f5f24739ecf9896c1
Myst33d <notmyst33d@gmail.com>
2021-08-06 23:51:38 +0600

    Initial commit

---
+++ youtube.js
@@ -0,0 +1,17 @@
+var oldSongArtist = ''
+var oldSongName = ''
+
+setInterval(function() {
+    try {
+        var songArtist = document.getElementById('upload-info').getElementsByTagName('a')[0].innerText
+        var songName = document.getElementById('info-contents').getElementsByClassName('title')[0].innerText
+
+        if (oldSongArtist != songArtist && oldSongName != songName) {
+            var http = new XMLHttpRequest()
+            http.open('POST', 'http://127.0.0.1:50142', true)
+            http.send(JSON.stringify({ artist: songArtist, name: songName }))
+            oldSongArtist = songArtist
+            oldSongName = songName
+        }
+    } catch {}
+}, 500)
